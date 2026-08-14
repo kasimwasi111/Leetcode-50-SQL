68289abfb0c8708c155f8dd0abfb22df8a63fc10
Kasim Wasi <96281559+kasimwasi111@users.noreply.github.com>
2026-08-15 01:45:10 +0530

    Time: 312 ms (17.56%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,3 +1,11 @@
+-- select a1.machine_id ,round(
+--     (select avg(a2.timestamp) from Activity a2 where a2.activity_type='end' and a2.machine_id  =a1.machine_id  )
+--     -
+--     (select avg(a2.timestamp) from Activity a2 where a2.activity_type='start' and a2.machine_id =a1.machine_id )
+--     ,3) as processing_time
+-- from Activity a1
+-- group by a1.machine_id ;
+
 # Select the machine ID
 SELECT s.machine_id,
 # Calculate the average processing time
