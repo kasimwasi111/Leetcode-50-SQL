34bf5df64b06afe38da9be34fe11794619fb6a21
Kasim Wasi <96281559+kasimwasi111@users.noreply.github.com>
2026-08-18 01:36:37 +0530

    Time: 469 ms (76.93%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,18 @@
-# Select today's ID because we want the ID of the rising-temperature day
-SELECT today.id
-# Use Weather table and call this copy "today"
-FROM Weather today
-# Use Weather table AGAIN and call this copy "yesterday"
-JOIN Weather yesterday
-# Make sure today is exactly one day after yesterday
-ON today.recordDate = DATE_ADD(yesterday.recordDate, INTERVAL 1 DAY)
-# Check whether today's temperature is higher than yesterday's
-WHERE today.temperature > yesterday.temperature;
+-- # Select today's ID because we want the ID of the rising-temperature day
+-- SELECT today.id
+-- # Use Weather table and call this copy "today"
+-- FROM Weather today
+-- # Use Weather table AGAIN and call this copy "yesterday"
+-- JOIN Weather yesterday
+-- # Make sure today is exactly one day after yesterday
+-- ON today.recordDate = DATE_ADD(yesterday.recordDate, INTERVAL 1 DAY)
+-- # Check whether today's temperature is higher than yesterday's
+-- WHERE today.temperature > yesterday.temperature;
+select id 
+from (select id,recordDate ,temperature ,
+lag(temperature) over (order by recordDate) as previoustemp,
+lag(recordDate) over (order by recordDate) as prevRecordDate
+from weather) w
+where temperature>previoustemp
+and datediff(recordDate,prevRecordDate)=1
+;
