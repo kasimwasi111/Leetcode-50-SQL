02ce276cd514ef6f19e78bb9bfbacad8167ceecd
Kasim Wasi <96281559+kasimwasi111@users.noreply.github.com>
2026-08-18 01:46:40 +0530

    Time: 293 ms (28.93%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,11 +1,3 @@
--- select a1.machine_id ,round(
---     (select avg(a2.timestamp) from Activity a2 where a2.activity_type='end' and a2.machine_id  =a1.machine_id  )
---     -
---     (select avg(a2.timestamp) from Activity a2 where a2.activity_type='start' and a2.machine_id =a1.machine_id )
---     ,3) as processing_time
--- from Activity a1
--- group by a1.machine_id ;
-
 # Select the machine ID
 SELECT s.machine_id,
 # Calculate the average processing time
@@ -21,8 +13,16 @@ ON s.machine_id = e.machine_id
 # Match records belonging to the same process
 AND s.process_id = e.process_id
 -- # Make sure s is the start record
-where s.activity_type = 'start'
+AND s.activity_type = 'start'
 -- # Make sure e is the end record
 AND e.activity_type = 'end'
 -- # Calculate the average separately for each machine
 GROUP BY s.machine_id;
+
+-- select a1.machine_id ,round(
+--     (select avg(a2.timestamp) from Activity a2 where a2.activity_type='end' and a2.machine_id  =a1.machine_id  )
+--     -
+--     (select avg(a2.timestamp) from Activity a2 where a2.activity_type='start' and a2.machine_id =a1.machine_id )
+--     ,3) as processing_time
+-- from Activity a1
+-- group by a1.machine_id ;
