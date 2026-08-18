75c127d32fa023747b115f7f0405c7ebcdae1012
Kasim Wasi <96281559+kasimwasi111@users.noreply.github.com>
2026-08-18 14:47:28 +0530

    Time: 867 ms (19.57%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,25 +1,29 @@
 # Select each user's ID
 SELECT 
     s.user_id,
-# Check whether action is 'confirmed'
-# TRUE = 1, FALSE = 0
-# AVG() gives the confirmation rate
-# IFNULL() changes NULL to 0
-# ROUND() keeps the result to 2 decimal places
+
+    # Count how many actions are 'confirmed'
+    # TRUE = 1, FALSE = 0
+    # Divide confirmed actions by total confirmation records
+    # IFNULL() changes NULL to 0
+    # ROUND() keeps the result to 2 decimal places
     ROUND(
         IFNULL(
-            sum(action = 'confirmed')/count(s.user_id),
+            SUM(action = 'confirmed') / COUNT(c.user_id),
             0
         ),
         2
     ) AS confirmation_rate
+
 # Start with all users from Signups
 FROM Signups s
+
 # Bring confirmation records for each user
 # LEFT JOIN keeps users even if they have no confirmation
 LEFT JOIN Confirmations c
+
 # Match records using user_id
 ON s.user_id = c.user_id
+
 # Create one group for each user
-# So AVG() is calculated separately for every user
 GROUP BY s.user_id;
