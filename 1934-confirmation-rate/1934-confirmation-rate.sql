c5c41dfb3fd5776f194704ffc1ad8833d04f1f96
Kasim Wasi <96281559+kasimwasi111@users.noreply.github.com>
2026-08-18 14:47:58 +0530

    Time: 721 ms (54.37%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,12 +1,11 @@
 # Select each user's ID
 SELECT 
     s.user_id,
-
-    # Count how many actions are 'confirmed'
-    # TRUE = 1, FALSE = 0
-    # Divide confirmed actions by total confirmation records
-    # IFNULL() changes NULL to 0
-    # ROUND() keeps the result to 2 decimal places
+# Count how many actions are 'confirmed'
+# TRUE = 1, FALSE = 0
+# Divide confirmed actions by total confirmation records
+# IFNULL() changes NULL to 0
+# ROUND() keeps the result to 2 decimal places
     ROUND(
         IFNULL(
             SUM(action = 'confirmed') / COUNT(c.user_id),
@@ -17,13 +16,10 @@ SELECT
 
 # Start with all users from Signups
 FROM Signups s
-
 # Bring confirmation records for each user
 # LEFT JOIN keeps users even if they have no confirmation
 LEFT JOIN Confirmations c
-
 # Match records using user_id
 ON s.user_id = c.user_id
-
 # Create one group for each user
 GROUP BY s.user_id;
